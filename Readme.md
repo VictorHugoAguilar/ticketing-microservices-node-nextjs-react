261d98d245faf247f2f6c3416664e48a91b4b96a
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-20 20:38:57 +0200

    create routes

---
+++ Readme.md
@@ -0,0 +1,23 @@
+
+
+
+# Docker Build Image of service
+
+````
+docker build -t {user}/auth .
+````
+
+
+# Setup nginx service for local dev
+
+Set-up local variable
+In Mac
+`````
+/etc/host
+add:
+127.0.0.1 ticketing.dev
+`````
+
+
+
+
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-users';
 
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
 
 app.listen( 3000, () => {
     console.log('Listen from port: ', 3000)
