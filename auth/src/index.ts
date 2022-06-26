862d338441f7fd6179ecfab5ce034af840eed489
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-26 18:15:15 +0200

    create mongo db docker

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-users';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
