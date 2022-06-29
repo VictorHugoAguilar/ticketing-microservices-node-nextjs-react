7eae20544b326eb64752f666f561803bd8a366f8
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-29 11:55:27 +0200

    common request validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there!');
-});
+router.post('/api/users/signin', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must suppy a password')
+],
+    validateRequest
+    , async (req: Request, res: Response) => {
+
+
+
+    });
 
 export { router as signinRouter };
