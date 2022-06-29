f9c0557220bbc8789355f2fb2f88939457670b14
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-29 12:57:10 +0200

    refactor class index, separate app to index

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,9 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
+import app from './app';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-users';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/singup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-})
-
-app.use(errorHandler);
 
 const start = async () => {
-    if(!process.env.JWT_KEY){
+    if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY not found, must be defined');
     }
 
