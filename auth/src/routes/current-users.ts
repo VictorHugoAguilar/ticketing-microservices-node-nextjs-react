17027c7c4f85b6e84dbc1f0fb0be11845ef8eb02
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-29 12:47:18 +0200

    creating a curren user middleware auth

--- auth/src/routes/current-users.ts
+++ auth/src/routes/current-users.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req: Request, res: Response) => {
     res.status(200).send({ currentUser: req.currentUser || null });
 });
 
-export { router as currentUserRouter };
+export { router as currentUserRouter }; 
