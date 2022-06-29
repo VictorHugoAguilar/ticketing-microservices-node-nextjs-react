8f4e9bda04ffc7a9fdbc067c5e4388c35244da35
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-29 12:38:01 +0200

    augmenting type definitions

--- auth/src/routes/current-users.ts
+++ auth/src/routes/current-users.ts
@@ -1,18 +1,11 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-    if (!req.session?.jwt) {
-        return res.send({ currentUser: null });
-    }
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        res.send({ currentUser: payload });
-    } catch (error) {
-        return res.send({ currentUser: null });
-    }
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
+    res.status(200).send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
