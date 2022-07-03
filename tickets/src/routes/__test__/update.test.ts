7c39f34adec8ca5b30b20cca762e84c0f4fdb6ee
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-03 18:30:23 +0200

    create new services update test

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+import app from '../../app';
+
+
+it('return 404 if the provided id does not exist', async () => { });
+
+it('return a 401 if the user is not authenticated', async () => { });
+
+it('return a 401 if the user does not own the ticket', async () => { });
+
+it('return a 400 if the user provides an invalid title or price', async () => { });
+
+it('updates the ticket provided valid', async () => { });
