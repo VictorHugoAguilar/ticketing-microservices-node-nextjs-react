a7d77b86acaa3e745ec72bcb435bff46a3340038
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-05 12:32:33 +0200

    implement controller new orders

--- orders/src/routes/news.ts
+++ orders/src/routes/news.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { requireAuth, validateRequest } from '@black_sheep/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+    body('ticketId')
+        .not()
+        .isEmpty()
+        .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+        .withMessage('TicketId must be provided')
+],
+    validateRequest,
+    async (req: Request, res: Response) => {
 
-    res.send({});
+        res.send({});
 
-});
+    });
 
 export { router as newOrderRouter };
