e056187867367030f50c4bacaa380891b5138d22
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-07 11:26:55 +0200

    Ticket Updated Listener Implementation

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,26 @@
+import { Listener, Subjects, TicketUpdatedEvent } from "@black_sheep/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    readonly subject = Subjects.TicketUpdated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message): void {
+        const ticket = await Ticket.findById('data.id');
+
+        if (!ticket) {
+            throw new Error('Ticket not found');
+        }
+
+        const { title, price } = data;
+        ticket.set({ title, price });
+
+        await ticket.save();
+
+        msg.ack();
+    }
+
+}
