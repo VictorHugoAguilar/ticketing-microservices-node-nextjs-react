cf06cb3d81049ad87cd93a59792170a71665c1ff
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-07 14:18:36 +0200

    Mock Function Arguments

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@black_sheep/common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../model/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 import { queueGrouName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -22,6 +23,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
         // Save the ticket
         await ticket.save();
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price: ticket.price,
+            title: ticket.title,
+            userId: ticket.userId,
+            orderId: ticket.orderId,
+            version: ticket.version,
+        });
 
         // ACK the message
         msg.ack();
