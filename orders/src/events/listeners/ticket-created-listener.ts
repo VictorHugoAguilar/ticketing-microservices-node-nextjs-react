6e02ffece08ef0b3e2f65c861c96e2c4a8317d6f
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-07 11:13:38 +0200

    siple onmessage implementation

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,13 +1,24 @@
 import { Listener, Subjects, TicketCreatedEvent } from "@black_sheep/common";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
 
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     readonly subject = Subjects.TicketCreated;
-    queueGroupName: string = 'orderes-service';
+    queueGroupName: string = queueGroupName;
 
-    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
+        const { title, price } = data;
 
+        const ticket = Ticket.build({
+            title,
+            price,
+        });
+
+        await ticket.save();
+
+        msg.ack();
     }
 
 }
