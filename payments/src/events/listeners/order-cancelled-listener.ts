17a004d4fa1ab41bed38a286e656992d6eef43cd
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-08 04:04:26 +0200

    implement events

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,28 @@
+import { Listener, OrderCancelledEvent, OrderStatus, Subjects } from "@black_sheep/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent>{
+    readonly subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+    queueGroupName: string = queueGroupName;
+
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+        const order = await Order.findOne({
+            _id: data.id,
+            version: data.version - 1
+        });
+
+        if (!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({
+            status: OrderStatus.Cancelled
+        });
+        await order.save();
+
+        msg.ack();
+    }
+
+}
