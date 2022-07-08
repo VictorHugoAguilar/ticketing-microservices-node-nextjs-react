51cea3f62c918c4f20f4a0062610fdbdba4cc9fc
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-08 06:44:15 +0200

    implements service front-end

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
         <div>
             <Header currentUser={currentUser} />
-            <Component {...pageProps} />
+            <Component currentUser={currentUser} {...pageProps} />
         </div>
     );
 };
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,9 @@
-import buildClient from "../api/build-client";
-
 const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You are signed in </h1> : <h1>Your not signed in</h1>
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-
-  return data;
+  return {};
 };
 
 export default LandingPage;
