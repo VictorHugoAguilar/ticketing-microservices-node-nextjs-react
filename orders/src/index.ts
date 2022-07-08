a22c2ab0c43a80b66e1cad4af0405782f39fabf4
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-08 03:18:12 +0200

    Listening for Expiration

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import { natsWrapper } from './nats-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 
 const start = async () => {
@@ -44,6 +45,7 @@ const start = async () => {
 
         new TicketCreatedListener(natsWrapper.client).listen();
         new TicketUpdatedListener(natsWrapper.client).listen();
+        new ExpirationCompleteListener(natsWrapper.client).listen();
 
         // connect to mongodb
         await mongoose.connect(process.env.MONGO_URI, {});
