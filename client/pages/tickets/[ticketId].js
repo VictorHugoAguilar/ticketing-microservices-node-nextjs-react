da1b29ed015b80fd2a21f094d2d767cc66d9ea26
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-07-09 21:30:04 +0200

    The Expiration Timer

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -2,28 +2,24 @@ import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 const TicketShow = ({ ticket }) => {
-
     const { doRequest, errors } = useRequest({
         url: '/api/orders',
         method: 'post',
         body: {
-            ticketId: ticket.id
+            ticketId: ticket.id,
         },
-        onSuccess: (order) => {
-            console.log(order);
-            Router.push('/orders/[orderId]', '/orders/${order.id}');
-        }
+        onSuccess: (order) =>
+            Router.push('/orders/[orderId]', `/orders/${order.id}`),
     });
 
     return (
         <div>
             <h1>{ticket.title}</h1>
-            <h4>{ticket.price} EUR</h4>
+            <h4>Price: {ticket.price}</h4>
             {errors}
-            <button
-                onClick={doRequest}
-                className="btn btn-primary"
-            >Purchase</button>
+            <button onClick={doRequest} className="btn btn-primary">
+                Purchase
+            </button>
         </div>
     );
 };
